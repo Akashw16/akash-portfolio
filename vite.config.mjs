51f5e6a9e822eaaf7e282692851e0814f72c5125
Akashw16 <iamakashw16@gmail.com>
2025-09-17 02:20:06 +0530

    new

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/akash-portfolio/', // <<--- Add this line!
-})
-=======
-  base: '/akash-portfolio/', 
-})
->>>>>>> f2f7e2fa5d969a19a58253a5782eeb433528951d
+  base: "/akash-portfolio/", // Required for GitHub Pages
+});
